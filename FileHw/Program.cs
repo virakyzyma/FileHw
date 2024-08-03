6527d2ccafb782b6bc513950494c550d3b6a9f83
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-08-03 08:15:31 +0300

    Add project files.

---
+++ FileHw/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+namespace FileHw
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string fileName = "File.txt";
+            using (FileStream fs = File.Create(fileName))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(fileName);
+                fs.Write(info, 0, info.Length);
+            }
+            using (StreamReader st = File.OpenText(fileName))
+            {
+                string str = "";
+                while ((str = st.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
